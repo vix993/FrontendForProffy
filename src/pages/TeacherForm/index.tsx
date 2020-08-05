34b9169a8df8b816d904e348227bcef7758b12e1
Victor <victor.as.nascimento@gmail.com>
2020-08-05 10:05:00 -0300

    implement and stylize teacher registration

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-        <PageHeader title="Thank you for sharing your skills." />
+        <PageHeader
+         title="Thank you for sharing your skills."
+         description="Step One: Fill in the onboarding form."
+        />
+        <main>
+            <fieldset>
+                <legend>Your Information</legend>
+                <Input name="name" label="Full Name" />
+                <Input name="avatar" label="Profile Picture" />
+                <Input name="whatsapp" label="Whatsapp" />
+            </fieldset>
+
+            <fieldset>
+                <legend>About Your Class</legend>
+                <Input name="subject" label="Subject" />
+                <Input name="cost" label="Your classes hourly cost" />
+            </fieldset>
+            <footer>
+                <p>
+                    <img src={warningIcon} alt="Important Warning"/>
+                    Warning! <br />
+                    Please, fill in all of the information
+                </p>
+                <button type="button">
+                    Register
+                </button>
+            </footer>
+        </main>
     </div>
     );
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,26 +4,16 @@ import TeacherItem from '../../components/TeacherItem';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
+import Input from '../../components/Input';
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available proffys.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Subject</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week-day">Weekday</label>
-                        <input type="text" id="week-day" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Time</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input name="subject" label="Subject" />
+                    <Input name="week_day" label="Week Day" />
+                    <Input type="time" name="time" label="Time" />
                 </form>
             </PageHeader>
             <main>
